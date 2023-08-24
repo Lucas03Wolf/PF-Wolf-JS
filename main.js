a1f814807ab3e9b98fa877308791ecd6f4cd947a
Lucas <lucas03wolf@gmail.com>
2023-08-23 23:50:27 -0300

    third commit

--- main.js
+++ main.js
@@ -1,63 +1,74 @@
-const listaProductos = [
+const productList = [
     {id:1, nombre:"Iphone X", precio:500, stock:10},
     {id:2, nombre:"Iphone 11", precio:550, stock:10},
     {id:3, nombre:"Iphone 12", precio:600, stock:10},
     {id:4, nombre:"Iphone 13", precio:750, stock:10},
-    {id:5, nombre:"Iphone 14", precio:850, stock:10}
+    {id:5, nombre:"Iphone 14", precio:850, stock:10},
 ]
 
-const carrito = []
+productList.forEach((item) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <h2>${item.nombre}</h2>
+    <p>Precio ${item.precio}<p>
+    <button id="boton${item.id}">Agregar</button>
+    <br/>`;
 
-class llenarCarrito{
-    constructor(objeto, cantidad){
-        this.id = objeto.id
-        this.nombre = objeto.nombre
-        this.precio = objeto.precio
-        this.stock = objeto.stock
-        this.cantidad = cantidad
-    }
-}
+    document.body.append(div);
+    
+    let button = document.getElementById(`boton${item.id}`)
+    button.addEventListener("click", () => add(item.id))
+});
 
-const añadirProducto = (nombre, cantidad = 1) => {
-    const producto = listaProductos.find(e => e.nombre === nombre)
-    if(!producto){
-        return "Ese producto no existe"
-    }
-    if(cantidad > producto.stock){
-        return "No disponemos de stock suficiente para ese producto"
-    }
-    const productos = carrito.find(e => e.nombre === nombre)
-    if(productos){
-        productos.cantidad += cantidad
-        productos.stock -= cantidad
-    }
-    else{
-        producto.stock -= cantidad
-        carrito.push(new llenarCarrito(producto, cantidad))
-    }
-    return carrito
-}
+let divCart = document.createElement("div");
+    divCart.innerHTML = `<h1>Carrito:</h1>`;
+    document.body.append(divCart);
 
-let seleccion = prompt(`Hola desea comprar algún producto?`)
+let divDelete = document.createElement("div");
+    divDelete.innerHTML = `<button>Eliminar carrito</button>`;
+    document.body.append(divDelete)
 
-if(seleccion != "si" && seleccion != "no"){
-    alert(`Por favor ingrese si o no`)
-}else if(seleccion == "si"){
-    alert(`A continucación nuestra lista de productos`)
-    let todosLosProductos = listaProductos.map((producto) => `ID: ${producto.id} - ${producto.nombre} - ${producto.precio}$`);
-    alert(todosLosProductos.join(" / "))
+    divDelete.addEventListener("click", () => {
+        localStorage.clear();
+        location.reload()
+    });
 
-    while(seleccion === "si"){     
-        let productoAComprar = prompt(`Agrega un producto a tu carrito`)
-        let unidades = parseInt(prompt(`Cuantas unidades quiere llevar`))
-        if(isNaN(unidades)){alert(`Ingrese un numero`)}
-        seleccion = prompt(`Desea seguir comprando?`)
-        console.log(añadirProducto(productoAComprar, unidades))
-    }
-    if(seleccion === "no"){
-        alert("Gracias por tu compra!")
-    }
+const saveLocal = (key, item) => {
+    localStorage.setItem(key, JSON.stringify(item));
+};
 
-}else if(seleccion == "no"){
-    alert(`Gracias por venir, hasta pronto!`)
-}
+let cart = []
+
+let cartStorage = JSON.parse(localStorage.getItem("cart"))
+
+if(cartStorage){
+    cart = cartStorage
+
+    cart.forEach((item) => {
+       let div = document.createElement("div");
+       div.innerHTML = `
+       <h2>${item.nombre}</h2>
+       <p>Precio ${item.precio}<p>
+       <br/>`;
+   
+       document.body.append(div);
+   });
+   }else{
+       let div = document.createElement("div");
+       div.innerHTML = `<h2>No hay productos en el carrito</h2>`;
+       document.body.append(div);
+   }
+
+const add = (id) => {
+    let product = productList.find((item) => item.id === id);
+    cart.push(product);
+    saveLocal("cart", cart)
+    location.reload()
+    let div = document.createElement("div");
+       div.innerHTML = `
+       <h2>${product.nombre}</h2>
+       <p>Precio ${product.precio}<p>
+       <br/>`;
+   
+       document.body.append(div);
+};
